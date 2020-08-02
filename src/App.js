b2c055c66a7aaf51c547355012057100ead0f3b1
Suryakumar Arumugam <asdsurya@gmail.com>
2020-08-02 12:37:28 +0530

    Implemented API fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api/index";
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchData });
+  }
+
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
         <h1>From JS Mastery</h1>
+        <Cards data={this.state.data} />
+        <Chart />
+        <CountryPicker />
       </div>
     );
   }
